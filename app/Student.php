3084ee20dc5547bfb96ed924b0b9f4539ea66931
Luis Manuel Ramirez Vargas <luis.manuel.ramirez.91@gmail.com>
2016-02-27 10:32:24 -0600

    Add student model

---
+++ app/Student.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace SchoolApp;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model
+{
+    //
+}
---
+++ database/migrations/2016_02_27_160704_create_students_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre');
+            $table->string('apellidos');
+            $table->string('genero');
+            $table->date('fecha_nacimiento');
+            $table->string('numero_celular');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('students');
+    }
+}
